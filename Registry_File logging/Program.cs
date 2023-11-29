5967d7c279200484278db95e41be6e672e62d9f2
Kostya Popov <kepel2823@gmail.com>
2023-11-29 20:37:18 +0200

    Add project files.

---
+++ Registry_File logging/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Win32;
+
+var subKey = "Software_regedit";
+
+var key = Registry.CurrentUser.OpenSubKey(subKey);
+if (key is null)
+{
+    var newKey = Registry.CurrentUser.CreateSubKey(subKey);
+    newKey.SetValue("First_Run", DateTime.Now.ToString());
+    newKey.Close();
+}
+
+var directoryPath = @"c:\test";
+var fileName = @"test1.txt";
+var fullPath = Path.Combine(directoryPath, fileName);
+
+var existsDir = Directory.Exists(directoryPath);
+var existsFile = File.Exists(fullPath);
+
+if (!existsDir) Directory.CreateDirectory(directoryPath);
+if (!existsFile) File.Create(fullPath).Close();
+
+using StreamWriter sw = File.AppendText(fullPath);
+sw.WriteLine($"Start program: {DateTime.Now}");
+Console.WriteLine("The information about the current run was successfully added to the file.");
